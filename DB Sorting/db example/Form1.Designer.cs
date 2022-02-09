59ad4776765688bbf108926e40aaf61f12970ccd
Justin Smith <justinrsmithresume@gmail.com>
2022-02-09 12:44:48 -0700

    Add files via upload

---
+++ DB Sorting/db example/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.sku = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sku
+            // 
+            this.sku.Location = new System.Drawing.Point(12, 12);
+            this.sku.Name = "sku";
+            this.sku.Size = new System.Drawing.Size(159, 20);
+            this.sku.TabIndex = 0;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(210, 12);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(106, 23);
+            this.send.TabIndex = 1;
+            this.send.Text = "send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(758, 11);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(131, 42);
+            this.exit.TabIndex = 2;
+            this.exit.Text = "exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 65F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 67);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(897, 300);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.send;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 388);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.sku);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sku;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DB Sorting/db example/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Data.OleDb;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            sku.Focus();
+        }
+        OleDbConnection connect = new OleDbConnection();
+        private void send_Click(object sender, EventArgs e)
+        {
+            
+            connect.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\\MOOTADOE-PC\Downloads\db1.mdb";
+            string skuz = sku.Text;
+            connect.Open();
+            OleDbCommand cmd = new OleDbCommand("INSERT into userdata(sku)" + "values(@skuz)", connect);
+
+
+
+
+
+
+            DataTable dt = new DataTable();
+            OleDbDataAdapter SDA = new OleDbDataAdapter("SELECT * FROM userdata where Sku like " + sku.Text, connect);
+            SDA.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connect.Close();
+            sku.Text = "";
+
+
+            try
+            {
+                bool fart = dt.Rows[0].Equals(DBNull.Value);
+            }
+            catch
+            {
+                connect.Open();
+                cmd.Parameters.Add("@skuz", OleDbType.Char, 20).Value = skuz;
+
+                cmd.ExecuteNonQuery();
+                DataTable dt2 = new DataTable();
+                OleDbDataAdapter SDA2 = new OleDbDataAdapter("SELECT * FROM userdata where Sku like " + skuz, connect);
+                SDA2.Fill(dt2);
+                dataGridView1.DataSource = dt2;
+                connect.Close();
+                sku.Text = "";
+            }
+            
+            
+
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+            
+    }
+}
+
